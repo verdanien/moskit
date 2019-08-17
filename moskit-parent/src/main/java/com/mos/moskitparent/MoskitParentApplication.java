e6648e1e15a93ca7805f295c0b15ff1289d38a2e
mmosor <marmosmar@gmail.com>
2019-08-17 16:11:43 +0200

    initial commit

---
+++ moskit-parent/src/main/java/com/mos/moskitparent/MoskitParentApplication.java
@@ -0,0 +1,13 @@
+package com.mos.moskitparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoskitParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoskitParentApplication.class, args);
+	}
+
+}
